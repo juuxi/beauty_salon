542713adf5fe1ea071a476980966c8e814b91a9f
juuxi <dimashopyrev@mail.ru>
2026-04-17 00:45:51 +0300

    impl new serializer for Value

--- beauty_salon/api/serializers.py
+++ beauty_salon/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from .models import ClassifierNode, Enumeration, Value
+from .models import StringData, IntData, RealData, PictureData, ContentType
 
 
 class ClassifierNodeSerializer(serializers.ModelSerializer):
@@ -24,11 +25,87 @@ class EnumerationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Enumeration
-        fields = ('id', 'name', 'measuring_unit')
+        fields = ('id', 'name', 'measuring_unit',
+                  'data_type')
 
 
 class ValueSerializer(serializers.ModelSerializer):
+    data = serializers.JSONField()
 
     class Meta:
         model = Value
-        fields = ('id', 'data', 'num', 'enumeration')
+        fields = ('id', 'data', 'num', 'enumeration',
+                  'data')
+
+    def validate_data(self, data):
+        enumeration_id = self.initial_data.get('enumeration')
+        data_type = Enumeration.objects.get(id=enumeration_id).data_type
+
+        if data_type == 'int':
+            if not isinstance(data, int):
+                raise serializers.ValidationError({
+                    'data': f'Expected integer for type "int", \
+                    got {type(data).__name__}'
+                })
+
+        elif data_type == 'str':
+            if not isinstance(data, str):
+                raise serializers.ValidationError({
+                    'data': f'Expected string for type "str", \
+                    got {type(data).__name__}'
+                })
+
+        elif data_type == 'real':
+            if not isinstance(data, (int, float)):
+                raise serializers.ValidationError({
+                    'data': f'Expected number for type "real", \
+                    got {type(data).__name__}'
+                })
+            data['data'] = float(data)
+
+        elif data_type == 'pic':
+            if not isinstance(data, str):
+                raise serializers.ValidationError({
+                    'data': 'Picture must be provided as URL'
+                })
+            if not data.startswith(('http://', 'https://')):
+                raise serializers.ValidationError({
+                    'data': 'Picture must be URL'
+                })
+        else:
+            raise serializers.ValidationError({
+                'data_type': f'Unknown data type: {data_type}'
+            })
+
+        return data
+
+    def create(self, validated_data):
+        data = validated_data.pop('data')
+        enumeration = self.validated_data['enumeration']
+        data_type = enumeration.data_type
+
+        if data_type == 'int':
+            data_obj = IntData.objects.create(data=data)
+        elif data_type == 'str':
+            data_obj = StringData.objects.create(data=data)
+        elif data_type == 'real':
+            data_obj = RealData.objects.create(data=data)
+        elif data_type == 'pic':
+            data_obj = PictureData.objects.create(image=data)
+        else:
+            raise serializers.dataError(f"Unknown type: {data_type}")
+
+        content_type = ContentType.objects.get_for_model(data_obj)
+
+        value_obj = Value.objects.create(
+            content_type=content_type,
+            data_object_id=data_obj.id,
+            **validated_data
+        )
+
+        return value_obj
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['data'] = instance.data.data if instance.data else None
+        return ret
