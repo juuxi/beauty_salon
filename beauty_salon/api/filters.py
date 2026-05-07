64b1473c15ecc624ff7cefd3366365191f767526
juuxi <dimashopyrev@mail.ru>
2026-05-07 17:26:10 +0300

    parameter.data_type-based approach impl

--- beauty_salon/api/filters.py
+++ beauty_salon/api/filters.py
@@ -2,6 +2,7 @@ import django_filters
 from django.db.models import Q
 from .models import Service, ParameterValueService, Parameter
 from .models import IntData, RealData, StringData, Value
+from .models import Enumeration
 
 from rest_framework.exceptions import ValidationError
 
@@ -9,15 +10,37 @@ from rest_framework.exceptions import ValidationError
 class ServiceFilter(django_filters.FilterSet):
     values = django_filters.CharFilter(method='filter_by_resolved_data')
 
+    def get_condition(self, param_value, param, exp_type, c_type_id, model):
+        try:
+            param_value = exp_type(param_value)
+        except ValueError:
+            raise ValidationError({'values': f'value for param \
+                                    {param.name} must \
+                                    be type {exp_type} \
+                                    got {type(param_value).__name__}'})
+
+        return (  # возвращает не по конкретеному параметру, а в общем
+            Q(content_type_id=c_type_id,
+                data_object_id__in=(model.objects
+                                    .filter(data=param_value)))
+        )
+
     def filter_by_resolved_data(self, queryset, name, value):
         if not value:
             return queryset
 
-        values = [v.strip() for v in str(value).split('&') if v.strip()]
+        items_dict = dict(self.data.items())
+        first_pair = items_dict['values']
+        try:
+            param, param_value = first_pair.split('=')
+        except ValueError:
+            raise ValidationError({'values': 'format is param=value'})
+        items_dict[param] = param_value
 
-        for value in values:
+        for param, param_value in items_dict.items():
+            if param == 'values':
+                continue
             try:
-                param, param_value = value.split('=')
                 param = Parameter.objects.get(name=param)
             except ValueError:
                 raise ValidationError({'values': 'format is param=value'})
@@ -25,39 +48,48 @@ class ServiceFilter(django_filters.FilterSet):
                 raise ValidationError({'values':
                                        'no parameter with this name'})
 
-            return queryset
+            condition = None
+            if param.data_type == 'int':
+                condition = self.get_condition(
+                    param_value, param, int, 17, IntData
+                )
 
-            # PATH 1: Direct terminate tables
-            direct_condition = (
-                Q(content_type_id=17,
-                  data_object_id__in=IntData.objects.filter(data__in=values)) |
-                Q(content_type_id=18,
-                  data_object_id__in=RealData.objects.filter(data__in=values))
-            )
+            if param.data_type == 'real':
+                condition = self.get_condition(
+                    param_value, param, float, 18, RealData
+                )
 
-            # PATH 2: Relay table that points to terminate tables
-            # Find relay records that point to matching terminate data
-            relay_q = (
-                Q(content_type_id=11, data_object_id__in=Value.objects.filter(
-                    content_type_id=17,
-                    data_object_id__in=IntData.objects.filter(data__in=values)
-                )) |
-                Q(content_type_id=11, data_object_id__in=Value.objects.filter(
-                    content_type_id=18,
-                    data_object_id__in=RealData.objects.filter(data__in=values)
-                )) |
-                Q(content_type_id=11, data_object_id__in=Value.objects.filter(
-                    content_type_id=19,
-                    data_object_id__in=(StringData.objects
-                                        .filter(data__in=values))
-                ))
-            )
+            if param.data_type == 'enum':
+                enumeration = Enumeration.objects.get(
+                    id=param.enumeration_id
+                )
 
-            # Combine paths & fetch Service IDs from the through model
+                if enumeration.data_type == 'int':
+                    condition = self.get_condition(
+                        param_value, param, int, 17, IntData
+                    )
+
+                if enumeration.data_type == 'real':
+                    condition = self.get_condition(
+                        param_value, param, float, 18, RealData
+                    )
+
+                if enumeration.data_type == 'str':
+                    condition = self.get_condition(
+                        param_value, param, str, 19, StringData
+                    )
+
+                condition = Q(
+                    content_type_id=11,
+                    data_object_id__in=Value.objects.filter(
+                        condition
+                    )
+                )
+
             matching_service_ids = (ParameterValueService.objects
-                                    .filter(direct_condition | relay_q)
-                                    .values_list('service', flat=True).
-                                    distinct())
+                                    .filter(condition)
+                                    .values_list('service', flat=True)
+                                    .distinct())
 
             queryset = queryset.filter(id__in=matching_service_ids)
 
