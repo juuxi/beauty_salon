3dce40856100caef89a3cfcd1f205fef6d90302b
juuxi <dimashopyrev@mail.ru>
2026-05-07 16:09:36 +0300

    filter format change with validation on parameter name part
    ?values=<param1>=<value1>&<param2>=<value2>

--- beauty_salon/api/filters.py
+++ beauty_salon/api/filters.py
@@ -1,7 +1,9 @@
 import django_filters
 from django.db.models import Q
-from .models import Service, ParameterValueService
+from .models import Service, ParameterValueService, Parameter
 from .models import IntData, RealData, StringData, Value
+
+from rest_framework.exceptions import ValidationError
 
 
 class ServiceFilter(django_filters.FilterSet):
@@ -13,37 +15,53 @@ class ServiceFilter(django_filters.FilterSet):
 
         values = [v.strip() for v in str(value).split('&') if v.strip()]
 
-        # PATH 1: Direct terminate tables
-        direct_condition = (
-            Q(content_type_id=17,
-              data_object_id__in=IntData.objects.filter(data__in=values)) |
-            Q(content_type_id=18,
-              data_object_id__in=RealData.objects.filter(data__in=values))
-        )
+        for value in values:
+            try:
+                param, param_value = value.split('=')
+                param = Parameter.objects.get(name=param)
+            except ValueError:
+                raise ValidationError({'values': 'format is param=value'})
+            except Parameter.DoesNotExist:
+                raise ValidationError({'values':
+                                       'no parameter with this name'})
 
-        # PATH 2: Relay table that points to terminate tables
-        # Find relay records that point to matching terminate data
-        relay_q = (
-            Q(content_type_id=11, data_object_id__in=Value.objects.filter(
-                content_type_id=17,
-                data_object_id__in=IntData.objects.filter(data__in=values)
-            )) |
-            Q(content_type_id=11, data_object_id__in=Value.objects.filter(
-                content_type_id=18,
-                data_object_id__in=RealData.objects.filter(data__in=values)
-            )) |
-            Q(content_type_id=11, data_object_id__in=Value.objects.filter(
-                content_type_id=19,
-                data_object_id__in=StringData.objects.filter(data__in=values)
-            ))
-        )
+            return queryset
 
-        # Combine paths & fetch Service IDs from the through model
-        matching_service_ids = (ParameterValueService.objects
-                                .filter(direct_condition | relay_q)
-                                .values_list('service', flat=True).distinct())
+            # PATH 1: Direct terminate tables
+            direct_condition = (
+                Q(content_type_id=17,
+                  data_object_id__in=IntData.objects.filter(data__in=values)) |
+                Q(content_type_id=18,
+                  data_object_id__in=RealData.objects.filter(data__in=values))
+            )
 
-        return queryset.filter(id__in=matching_service_ids)
+            # PATH 2: Relay table that points to terminate tables
+            # Find relay records that point to matching terminate data
+            relay_q = (
+                Q(content_type_id=11, data_object_id__in=Value.objects.filter(
+                    content_type_id=17,
+                    data_object_id__in=IntData.objects.filter(data__in=values)
+                )) |
+                Q(content_type_id=11, data_object_id__in=Value.objects.filter(
+                    content_type_id=18,
+                    data_object_id__in=RealData.objects.filter(data__in=values)
+                )) |
+                Q(content_type_id=11, data_object_id__in=Value.objects.filter(
+                    content_type_id=19,
+                    data_object_id__in=(StringData.objects
+                                        .filter(data__in=values))
+                ))
+            )
+
+            # Combine paths & fetch Service IDs from the through model
+            matching_service_ids = (ParameterValueService.objects
+                                    .filter(direct_condition | relay_q)
+                                    .values_list('service', flat=True).
+                                    distinct())
+
+            queryset = queryset.filter(id__in=matching_service_ids)
+
+        return queryset
 
     class Meta:
         model = Service
