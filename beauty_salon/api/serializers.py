a65241356af75bb666b239e44c5187cdbdadf40e
juuxi <dimashopyrev@mail.ru>
2026-05-14 19:06:45 +0300

    add code field to OperationsClassifierSerializer

--- beauty_salon/api/serializers.py
+++ beauty_salon/api/serializers.py
@@ -298,7 +298,7 @@ class SubjectCategorySerializer(serializers.ModelSerializer):
 class OperationsClassifierSerializer(serializers.ModelSerializer):
     class Meta:
         model = OperationsClassifier
-        fields = ('id', 'name', 'subject_roles', 'document_roles')
+        fields = ('id', 'name', 'subject_roles', 'document_roles', 'code')
 
 
 class SubjectRoleSerializer(serializers.ModelSerializer):
