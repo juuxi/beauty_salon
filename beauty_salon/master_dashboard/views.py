0b268fe858c718f47bb1bbe1d60c9559a893a5a7
juuxi <dimashopyrev@mail.ru>
2026-05-15 12:22:52 +0300

    properly configure index + services impl

--- beauty_salon/master_dashboard/views.py
+++ beauty_salon/master_dashboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+
+from api.models import Service
+
+
+class ServiceListView(ListView):
+    model = Service
+    template_name = 'services.html'
+    context_object_name = 'services'
