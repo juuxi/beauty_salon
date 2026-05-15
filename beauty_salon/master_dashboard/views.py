a570eb6895df4c45d04dfd5cd35c2c46b5cf0767
juuxi <dimashopyrev@mail.ru>
2026-05-15 20:39:53 +0300

    order all models by id

--- beauty_salon/master_dashboard/views.py
+++ beauty_salon/master_dashboard/views.py
@@ -15,27 +15,32 @@ class ServiceListView(ListView):
     model = Service
     template_name = 'services.html'
     context_object_name = 'services'
+    ordering = 'id'
 
 
 class ClassifierNodeView(ListView):
     model = ClassifierNode
     template_name = 'classifier_nodes.html'
     context_object_name = 'classifier_nodes'
+    ordering = 'id'
 
 
 class ParameterView(ListView):
     model = Parameter
     template_name = 'parameters.html'
     context_object_name = 'parameters'
+    ordering = 'id'
 
 
 class EnumerationView(ListView):
     model = Enumeration
     template_name = 'enumerations.html'
     context_object_name = 'enumerations'
+    ordering = 'id'
 
 
 class MeasuringUnitView(ListView):
     model = MeasuringUnit
     template_name = 'measuring_units.html'
     context_object_name = 'measuring_units'
+    ordering = 'id'
