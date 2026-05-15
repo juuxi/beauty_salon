f2efd29f821fc3b4ffd088b59fe2234e105513ec
juuxi <dimashopyrev@mail.ru>
2026-05-15 13:03:22 +0300

    change routing paths

--- beauty_salon/master_dashboard/urls.py
+++ beauty_salon/master_dashboard/urls.py
@@ -7,9 +7,9 @@ app_name = 'master_dashboard'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('services/', views.ServiceListView.as_view(), name='services'),
-    path('classifier_nodes/', views.ClassifierNodeView.as_view(), name='classifier_nodes'),
-    path('parameters/', views.ParameterView.as_view(), name='parameters'),
-    path('enumerations/', views.EnumerationView.as_view(), name='enumerations'),
-    path('measuring_units/', views.MeasuringUnitView.as_view(), name='measuring_units'),
+    path('services/list/', views.ServiceListView.as_view(), name='services'),
+    path('classifier_nodes/list/', views.ClassifierNodeView.as_view(), name='classifier_nodes'),
+    path('parameters/list/', views.ParameterView.as_view(), name='parameters'),
+    path('enumerations/list/', views.EnumerationView.as_view(), name='enumerations'),
+    path('measuring_units/list/', views.MeasuringUnitView.as_view(), name='measuring_units'),
 ]
