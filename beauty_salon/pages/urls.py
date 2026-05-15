dfac0e784c27265610a1a75ea3e9e91a339e4b23
juuxi <dimashopyrev@mail.ru>
2026-05-15 20:36:35 +0300

    about/contacts impl

--- beauty_salon/pages/urls.py
+++ beauty_salon/pages/urls.py
@@ -6,4 +6,6 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contacts/', TemplateView.as_view(template_name='contacts.html'), name='contacts'),
 ]
