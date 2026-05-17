b34d54ad3441de5323628ba6cd2d40e2773ca2c3
juuxi <dimashopyrev@mail.ru>
2026-05-17 21:58:07 +0300

    pages templates moved to pages/

--- beauty_salon/pages/urls.py
+++ beauty_salon/pages/urls.py
@@ -5,7 +5,6 @@ app_name = 'pages'
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
-    path('contacts/', TemplateView.as_view(template_name='contacts.html'), name='contacts'),
+    path('', TemplateView.as_view(template_name='pages/index.html'), name='index'),
+    path('about/', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 ]
