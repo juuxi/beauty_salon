fe8c89357bc1d09f0c53fa06bb48609a7253d40c
juuxi <dimashopyrev@mail.ru>
2026-05-23 11:19:48 +0300

    registration impl

--- beauty_salon/pages/urls.py
+++ beauty_salon/pages/urls.py
@@ -3,6 +3,8 @@ from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_not_required
 from django.contrib.auth.views import LoginView, LogoutView
 
+from . import views
+
 app_name = 'pages'
 
 
@@ -14,6 +16,7 @@ urlpatterns = [
     ),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('register/', views.register_view, name='register'),
     path(
         '',
         login_not_required(TemplateView.as_view(template_name='pages/index.html')),
--- beauty_salon/pages/views.py
+++ beauty_salon/pages/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_not_required
 
-# Create your views here.
+
+@login_not_required
+def register_view(request):
+    form = UserCreationForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.save()
+            if user is not None:
+                login(request, user)
+                return redirect('pages:index')
+    return render(request, 'registration/register.html', {'form': form})
