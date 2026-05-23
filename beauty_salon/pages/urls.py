e336d3b12921d2ef06ba4ae267912dc178c9f47d
juuxi <dimashopyrev@mail.ru>
2026-05-23 11:03:03 +0300

    not require login for / and about/

--- beauty_salon/pages/urls.py
+++ beauty_salon/pages/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_not_required
 
 app_name = 'pages'
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='pages/index.html'), name='index'),
-    path('about/', TemplateView.as_view(template_name='pages/about.html'), name='about'),
+    path(
+        '',
+        login_not_required(TemplateView.as_view(template_name='pages/index.html')),
+        name='index'
+    ),
+    path(
+        'about/',
+        login_not_required(TemplateView.as_view(template_name='pages/about.html')),
+        name='about'
+    ),
 ]
