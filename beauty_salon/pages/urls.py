48526ac5e8e4b30238904ca79f8c5f80ae965c69
juuxi <dimashopyrev@mail.ru>
2026-05-23 11:08:27 +0300

    move login/logout to pages

--- beauty_salon/pages/urls.py
+++ beauty_salon/pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_not_required
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'pages'
 
@@ -16,4 +17,6 @@ urlpatterns = [
         login_not_required(TemplateView.as_view(template_name='pages/about.html')),
         name='about'
     ),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
