5088ae47bd4f90e2559d03eec5b26b06269239c5
juuxi <dimashopyrev@mail.ru>
2026-08-16 23:17:28 +0300

    refactor filtering for enum parameters
    current version prints ids instead of actual values in representation, but aside from that works fine, including both lte and gte

--- beauty_salon/api/filters.py
+++ beauty_salon/api/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from django.db.models import FloatField
 from django.db.models.functions import Cast
-from .models import Service, ParameterValueService, Parameter
+from .models import Service, ParameterValueService, Parameter, Value, Enumeration
 
 from rest_framework.exceptions import ValidationError
 
@@ -47,6 +47,44 @@ class ServiceFilter(django_filters.FilterSet):
             .distinct()
         )
 
+    def get_enum_value_ids(self, param_value, param, exp_type, mode, enumeration):
+        try:
+            param_value = exp_type(param_value)
+        except ValueError:
+            raise ValidationError({'values': f'value for param \
+                                    {param.name} must \
+                                    be type {exp_type} \
+                                    got {type(param_value).__name__}'})
+
+        if mode:
+            if exp_type != int and exp_type != float:
+                raise ValidationError({'values': 'mode is only availible \
+                                        for number params'})
+            if mode == 'gte':
+                return (
+                    Value.objects
+                    .filter(enumeration=enumeration)
+                    .annotate(data_num=Cast("data", output_field=FloatField()))
+                    .filter(data_num__gte=param_value)
+                )
+
+            if mode == 'lte':
+                return (
+                    Value.objects
+                    .filter(enumeration=enumeration)
+                    .annotate(data_num=Cast("data", output_field=FloatField()))
+                    .filter(data_num__lte=param_value)
+                )
+
+        if isinstance(param_value, float) and param_value.is_integer():
+            param_value = int(param_value)
+
+        return (
+            Value.objects
+            .filter(enumeration=enumeration)
+            .filter(data=param_value)
+        )
+
     def format_corruption_error(self):
         raise ValidationError({'values': 'format is param[__mode]=value'})
 
@@ -78,6 +116,8 @@ class ServiceFilter(django_filters.FilterSet):
             except Parameter.DoesNotExist:
                 raise ValidationError({'values': 'no parameter with this name'})
 
+            matching_service_ids = []
+
             if param.data_type == 'int':
                 matching_service_ids = self.get_matching_ids(param_value, param, int, mode)
 
@@ -85,7 +125,29 @@ class ServiceFilter(django_filters.FilterSet):
                 matching_service_ids = self.get_matching_ids(param_value, param, float, mode)
 
             if param.data_type == 'enum':
-                pass
+                enumeration = Enumeration.objects.get(id=param.enumeration_id)
+                t = None
+
+                if enumeration.data_type == 'int':
+                    t = int
+                if enumeration.data_type == 'real':
+                    t = float
+                if enumeration.data_type == 'str':
+                    t = str
+                if enumeration.data_type == 'pic':
+                    t = str
+
+                enum_values = self.get_enum_value_ids(
+                    param_value, param, t, mode, enumeration
+                )
+                for value_obj in enum_values:
+                    value_id = value_obj.id
+                    matching_service_ids = (
+                        ParameterValueService.objects
+                        .filter(value=value_id)
+                        .values_list('service', flat=True)
+                        .distinct()
+                    )
 
             queryset = queryset.filter(id__in=matching_service_ids)
 
