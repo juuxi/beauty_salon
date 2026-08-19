a580ca3f8195e632c44bd5c98cfd25dbdcebfda5
juuxi <dimashopyrev@mail.ru>
2026-08-19 10:18:46 +0300

    copypast api filtering into visual filtering

--- beauty_salon/master_dashboard/filters.py
+++ beauty_salon/master_dashboard/filters.py
@@ -1,6 +1,7 @@
 import django_filters
-from django.db.models import Q
-from api.models import Service, ParameterValueService, Parameter
+from django.db.models import FloatField
+from django.db.models.functions import Cast
+from api.models import Service, ParameterValueService, Parameter, Value, Enumeration
 
 from django.core.exceptions import ValidationError
 
@@ -8,31 +9,87 @@ from django.core.exceptions import ValidationError
 class ServiceFilter(django_filters.FilterSet):
     values = django_filters.CharFilter(method='filter_by_resolved_data')
 
-    def get_condition(self, param_value, param, exp_type, content_type_id, model, mode):
-        param_value = exp_type(param_value)
+    def get_matching_ids(self, param_value, param, exp_type, mode):
+        try:
+            param_value = exp_type(param_value)
+        except ValueError:
+            raise ValidationError({'values': f'value for param \
+                                    {param.name} must \
+                                    be type {exp_type} \
+                                    got {type(param_value).__name__}'})
 
         if mode:
             if exp_type != int and exp_type != float:
-                raise ValidationError('mode is only availible \
-                                        for number params')
+                raise ValidationError({'values': 'mode is only availible \
+                                        for number params'})
             if mode == 'gte':
-                return Q(
-                    content_type_id=content_type_id,
-                    data_object_id__in=(model.objects.filter(data__gte=param_value)),
-                )
-            if mode == 'lte':
-                return Q(
-                    content_type_id=content_type_id,
-                    data_object_id__in=(model.objects.filter(data__lte=param_value)),
+                return (
+                    ParameterValueService.objects
+                    .filter(parameter=param)
+                    .annotate(value_num=Cast("value", output_field=FloatField()))
+                    .filter(value_num__gte=param_value)
+                    .values_list('service', flat=True)
+                    .distinct()
                 )
 
-        return Q(
-            content_type_id=content_type_id,
-            data_object_id__in=(model.objects.filter(data=param_value)),
+            if mode == 'lte':
+                return (
+                    ParameterValueService.objects
+                    .filter(parameter=param)
+                    .annotate(value_num=Cast("value", output_field=FloatField()))
+                    .filter(value_num__lte=param_value)
+                    .values_list('service', flat=True)
+                    .distinct()
+                )
+
+        return (
+            ParameterValueService.objects
+            .filter(parameter=param)
+            .filter(value=param_value)
+            .values_list('service', flat=True)
+            .distinct()
+        )
+
+    def get_enum_value_ids(self, param_value, param, exp_type, mode, enumeration):
+        try:
+            param_value = exp_type(param_value)
+        except ValueError:
+            raise ValidationError({'values': f'value for param \
+                                    {param.name} must \
+                                    be type {exp_type} \
+                                    got {type(param_value).__name__}'})
+
+        if mode:
+            if exp_type != int and exp_type != float:
+                raise ValidationError({'values': 'mode is only availible \
+                                        for number params'})
+            if mode == 'gte':
+                return (
+                    Value.objects
+                    .filter(enumeration=enumeration)
+                    .annotate(data_num=Cast("data", output_field=FloatField()))
+                    .filter(data_num__gte=param_value)
+                )
+
+            if mode == 'lte':
+                return (
+                    Value.objects
+                    .filter(enumeration=enumeration)
+                    .annotate(data_num=Cast("data", output_field=FloatField()))
+                    .filter(data_num__lte=param_value)
+                )
+
+        if isinstance(param_value, float) and param_value.is_integer():
+            param_value = int(param_value)
+
+        return (
+            Value.objects
+            .filter(enumeration=enumeration)
+            .filter(data=param_value)
         )
 
     def format_corruption_error(self):
-        raise ValidationError('format is param[__mode]=value')
+        raise ValidationError({'values': 'format is param[__mode]=value'})
 
     def filter_by_resolved_data(self, queryset, name, value):
         if not value:
@@ -60,17 +117,40 @@ class ServiceFilter(django_filters.FilterSet):
             except ValueError:
                 self.format_corruption_error()
             except Parameter.DoesNotExist:
-                raise ValidationError('no parameter with this name')
+                raise ValidationError({'values': 'no parameter with this name'})
 
-            condition = Q(
-                parameter=param,
-            )
+            matching_service_ids = []
 
-            matching_service_ids = (
-                ParameterValueService.objects.filter(condition)
-                .values_list('service', flat=True)
-                .distinct()
-            )
+            if param.data_type == 'int':
+                matching_service_ids = self.get_matching_ids(param_value, param, int, mode)
+
+            if param.data_type == 'real':
+                matching_service_ids = self.get_matching_ids(param_value, param, float, mode)
+
+            if param.data_type == 'enum':
+                enumeration = Enumeration.objects.get(id=param.enumeration_id)
+                t = None
+
+                if enumeration.data_type == 'int':
+                    t = int
+                if enumeration.data_type == 'real':
+                    t = float
+                if enumeration.data_type == 'str':
+                    t = str
+                if enumeration.data_type == 'pic':
+                    t = str
+
+                enum_values = self.get_enum_value_ids(
+                    param_value, param, t, mode, enumeration
+                )
+                for value_obj in enum_values:
+                    value_id = value_obj.id
+                    matching_service_ids = (
+                        ParameterValueService.objects
+                        .filter(value=value_id)
+                        .values_list('service', flat=True)
+                        .distinct()
+                    )
 
             queryset = queryset.filter(id__in=matching_service_ids)
 
